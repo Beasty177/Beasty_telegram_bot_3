9753480dc2905ea9f71c54a56bf7690a5d91a591
Beasty pnx <pnx@bk.ru>
2024-12-08 16:37:21 +0300

    Сделал контроль имени бота из списка с возможными именами бота, и если имени нет, то повторяет сообщение с шапкой и подвалом

--- Beasty_telrgram_bot_3.py
+++ Beasty_telrgram_bot_3.py
@@ -10,12 +10,12 @@ random_hello=['Ну здарова, ёпта','Хули надо, кожаный
 
 link_main="https://t.me/vtornikshow"
 
-text_top_post = "[VTRNK Radio Show](https://t.me/vtornikshow)"
+text_top_post = {"VTRNK Radio Show":"https://t.me/vtornikshow"}
 
 vtrnk_data="10 декабря 2024"
 vtrnk_dj_nik = "Beasty"
 mes=[]
-bot_name = ["уебок","пиздюк", "ушлепок"]
+bot_name = ["уебок","пиздюк", "ушлепок", "уебок,","пиздюк,", "ушлепок,""уебок?","пиздюк?", "ушлепок?" ]
 
 @bot.message_handler(commands=['hello'])
 def rand_hello(message):
@@ -38,21 +38,23 @@ def hello_message (message):
 			bot.reply_to(message, task)
 			mes = None
 			return
-	print (mes)
 
+	#print (mes)
 	mes=None
+	echo_all(message)
+
+
 
 
 #@bot.message_handler(func=lambda message: True)
-#def echo_all(message):
-#
-#	#bot.reply_to(message, message.text)
-#	bot.send_message(message.chat.id, f'__Нижнее подчёркивание__ '
-#									  f'\n ~Зачёркнутый~ '
-#									  f'\n *text* '
-#									  f'\n ||spoiler|| '
-#									  f'\n *[VTRNK Radio Show](https://t.me/vtornikshow)*', parse_mode='MarkdownV2')
-#	bot.send_message(message.chat.id, f'{text_top_post} \n Прив прив прив \n {message.text} Hello, *world*')
+def echo_all(message):
+	bot.reply_to(message, message.text)
+	bot.send_message(message.chat.id, f'__Нижнее подчёркивание__ '
+									  f'\n ~Зачёркнутый~ '
+									  f'\n *text* '
+									  f'\n ||spoiler|| '
+									  f'\n *[VTRNK Radio Show](https://t.me/vtornikshow)*', parse_mode='MarkdownV2')
+
 
 
 	#print (message)
