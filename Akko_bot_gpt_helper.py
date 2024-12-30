3ac8ab5bdf21a5cff9b613b8f00c48ac710d5b02
Beasty pnx <pnx@bk.ru>
2024-12-30 15:40:13 +0300

    Привел код в порядок

--- Akko_bot_gpt_helper.py
+++ Akko_bot_gpt_helper.py
@@ -1,9 +1,5 @@
-import telebot
-#from telebot import formatting
-from random import choice
 from telebot import types
 import g4f
-#import provider
 from g4f.client import Client
 import telebot
 
@@ -17,13 +13,10 @@ bot_mess_id = None
 chat_GPT_switch= False
 text_main_menu_message='*Выбери нужный пункт*'
 text_main_welcome_menu = 'Привет я *Влади* \- твой бот помощник в Акко\! \n *Выбери нужный пункт в меню* \n Буду рад помочь\!'
-#Словарь для соотношения пользователей и их сообщений
+#Словарь для хранения id сообщений для удаления
 notes_users = {}
 
-mess_to_delete=[]
-
-
-# создаем клавиатуры
+# создаем клавиатуры и разные меню
 main_menu = types.InlineKeyboardMarkup()
 # добавляем на нее кнопки
 button1 = types.InlineKeyboardButton(text="Узнать информацию", callback_data="button1")
@@ -36,32 +29,12 @@ button7 = types.InlineKeyboardButton(text="Транспорт", callback_data="b
 main_menu.add(button1,button2, button3, button4, button5,button6, button7, row_width=1)
 
 #Клавиатура возврата в главное меню
-
 go_back_main_menu = types.InlineKeyboardMarkup()
 #button99= types.InlineKeyboardButton(text="Помощь", callback_data="button99")
 button100 = types.InlineKeyboardButton(text="Назад в главное меню", callback_data="button100")
 go_back_main_menu.add(button100,row_width=1)
 
 
-
-#Очистка чата с ботом
-#@bot.message_handler(commands=['clear'])
-#def delete_all_messages(message):
-#    chat_id = message.chat.id
-#    message_id = message.message_id
-#    messages_to_delete = []
-#
-#    messages = bot.get_chat_history(chat_id)
-#    for msg in messages:
-#        messages_to_delete.append(msg.message_id)
-#
-#    for msg_id in messages_to_delete:
-#        try:
-#            bot.delete_message(chat_id, msg_id)
-#        except Exception as e:
-#            print("", e)
-#
-#Конец очистки
 
 
 #Функция для добавления id сообщений в список для удаления
@@ -89,7 +62,7 @@ def delete_mes_add_list (c_id):
 
 
 
-#Главное меню
+#Главное меню вызывается командой /menu
 @bot.message_handler(commands=["menu"])
 def menu_call_mess(call):
     global  mess_id, chat_id, bot_mess_id, notes_users,chat_GPT_switch, mess_to_delete
@@ -107,134 +80,87 @@ def menu_call_mess(call):
     mes_clear_add_list (bot_msg.chat.id,bot_msg.message_id) #Функция для добавления id сообщений в список для удаления
 
 
-
-
-
-#
-#
- #   #Информация о сообщениях бота в каждом чате - что бы потом удалить нужную (сделана через словарь и список id сообщений)
- #   bot_mess_id = bot_msg.message_id #Получаем id текущего сообщения бота
-#  chat_id = bot_msg.chat.id #Получаем id текущего чата
- #   #mess_to_delete.append(bot_mess_id) #Записываем в список id сообщений бота для дальнейшего удаления если нужно
- #
- #
- #
- #   m=[]
-#
- #   if chat_id in notes_users:
- #       m=notes_users[chat_id]
- #       #print('m ', m)
-#
- #   m.append(bot_mess_id)
- #   notes_users[chat_id]=m
- #   print (notes_users)
-#
-#
-#
-#
-#
-
-
-
-#Меню и стартовое приветствие
+#Меню и стартовое приветствие вызывается командой /start
 @bot.message_handler(commands=["start"])
 def main_hello(call):
 
     # Обьявляем переменные глобальными
-    global mess_id, chat_id, notes_users, chat_GPT_switch
+    global  chat_GPT_switch
 
     chat_GPT_switch = False #Выключаем чат GPT
 
-    #  mes_clear_add_list(значение chat.id, значение message_id)
+    #  функция запоминания id сообщений mes_clear_add_list(значение chat.id, значение message_id)
     mes_clear_add_list(call.chat.id,call.message_id)  # Функция для добавления id сообщений пользователя (команды типа /start) в список для удаления
 
     # отправляем сообщение пользователю
-    bot_msg= bot.send_message(call.chat.id, f'{text_main_welcome_menu}'
-                                      ,parse_mode='MarkdownV2', reply_markup=main_menu)
+    bot_msg= bot.send_message(call.chat.id, f'{text_main_welcome_menu}',parse_mode='MarkdownV2', reply_markup=main_menu)
 
-    #  mes_clear_add_list(значение chat.id, значение message_id)
+    #  функция запоминания id сообщений которые послал бот mes_clear_add_list(значение chat.id, значение message_id)
     mes_clear_add_list(bot_msg.chat.id, bot_msg.message_id)  # Функция для добавления id сообщений в список для удаления
 
 
-    chat_id = call.chat.id
-    #mess_id = call.message_id
-    #bot_mess_id = bot_msg.message_id
-    # Удаляем команду старт если захотим
-    #bot.delete_message(chat_id, mess_id)
 
-    # Информация о сообщениях бота в каждом чате - что бы потом удалить нужную (сделана через словарь и список id сообщений)
-   # bot_mess_id = bot_msg.message_id  # Получаем id текущего сообщения бота
-   # chat_id = bot_msg.chat.id  # Получаем id текущего чата
-   # mess_to_delete.append(bot_mess_id)  # Записываем в список id сообщений бота для дальнейшего удаления если нужно
-#
-   # m = notes_users[chat_id] = []
-   # m.append(bot_mess_id)
-   # print(notes_users)
-
-    #print (f'в главном меню \n chat_id {chat_id}\n mess_id {mess_id} '
-    #       f'\n bot_mess_id {bot_mess_id}')
-
-
-# функция запустится, когда пользователь нажимает на кнопку
+# Реакция на нажатия кнопок в боте, когда пользователь нажимает на кнопку
 @bot.callback_query_handler(func=lambda call: True)
 
 def callback_inline(call):
-    global mess_id, chat_id, bot_mess_id, notes_users, chat_GPT_switch, mess_to_delete
+    global chat_GPT_switch
 
-    #print(f'в главном меню \n chat_id {chat_id}\n mess_id {mess_id}')
-
-    # bot.delete_message(chat_id=chat_id, message_id=mess_id, timeout=6)
 
     if call.message:
         if call.data == "button1":  #Переход в чат GPT
+
             chat_gpt(call)
 
         if call.data == "button2": #Отдел абсорбции Акко
-            absorb_office(call)
-            print(call.message.chat.id)
-            #print(call)
 
-            #print(call.message.json.chat.id)
+            absorb_office(call)
+            #print(call.message.chat.id) #Это id чата для кнопок
             #bot.send_message(call.message.chat.id, "Вы нажали на вторую кнопку.")
 
         if call.data == "button3": #Аренда квартиры
+
             arenda_kvartir(call)
             #bot.send_message(call.message.chat.id, "Вы нажали на третью кнопку.")
 
         if call.data == "button4": #Корзина абсорбции
+
             korzina_absorb(call)
             #bot.send_message(call.message.chat.id, "Вы нажали на четвертую кнопку.")
 
-        if call.data == "button5":
-            #Переход на страницу банка
-            #bot.send_message(call.message.chat.id, "Вы нажали на пятую кнопку.")
+        if call.data == "button5": #Переход на страницу банка
+
             bank_page(call)
+            #bot.send_message(call.message.chat.id, "Вы нажали на пятую кнопку.")
 
         if call.data == "button6": #Телефон и интернет
+
             telefon_internet(call)
             #bot.send_message(call.message.chat.id, "Вы нажали на шестую кнопку.")
 
         if call.data == "button7": #Транспорт
+
             tranport_rules(call)
             #bot.send_message(call.message.chat.id, "Вы нажали на седьмую кнопку.")
 
         if call.data == "button100": #Возврат в главное меню
-            main_bot_menu(call)
-            chat_GPT_switch = False
+
+            main_bot_menu(call) #Нажатие кнопки из другого меню для возврата в главное меню
+            chat_GPT_switch = False #Выключаем GPT
 
 
-#Главное меню
+#Главное меню вызывается кнопкой на клавиатуре из сообщений
 def main_bot_menu (call):
-    global mess_id, chat_id, bot_mess_id, notes_users, text_main_menu_message, mess_to_delete
-    print(call.message.chat.id)
-    delete_mes_add_list (call.message.chat.id) #Функция удаления ненужных сообщений из чата меню
 
+    #print(call.message.chat.id)
+    delete_mes_add_list (call.message.chat.id) #Функция удаления ненужных сообщений из чата меню передает значение id чата
 
-
-
+    #Сообщение главного меню с кнопками
     bot_msg = bot.send_message(call.message.chat.id, f'{text_main_menu_message}', parse_mode='MarkdownV2',
                                reply_markup=main_menu)
 
+    #  функция запоминания id сообщений которые послал бот mes_clear_add_list(значение chat.id, значение message_id)
+    mes_clear_add_list(bot_msg.chat.id, bot_msg.message_id)  # Функция для добавления id сообщений в список для удаления
 
 
 #Отдел абсорбции Акко
@@ -242,7 +168,6 @@ def absorb_office(call):
     bot.send_message(call.message.chat.id,
                      f"Рассказ о отделе абсорбции, его функциях, руководстве, контакты, полезная информация, возможность построения маршрута в навигаторе\. "
                      , parse_mode='MarkdownV2', reply_markup=go_back_main_menu)
-
 
 
 #Аренда квартиры
@@ -259,31 +184,18 @@ def korzina_absorb(call):
                      , parse_mode='MarkdownV2', reply_markup=go_back_main_menu)
 
 
-
-
 #Страница банка
 def bank_page (call):
-
-    #try:
-    #    bot.delete_message(chat_id, bot_mess_id) #Удаляем сообщение с главным меню
-    #except Exception as e:
-    #    print (e)
-
     bot.send_message(call.message.chat.id,f"В этом разделе может быть рассказано о времени работы банка, как записаться на прием, адрес, основные принципы и отличия банковской системы, что такое чековая книжка и тп \n"
                                           f"Так же тут можно быстро построить маршрут до банка, нажав на ссылку \n *[Построить маршрут до офиса Дисконт банка в Акко](https://maps.app.goo.gl/HPrqV1JQrk8SrWDMA)*"
                      , parse_mode='MarkdownV2', reply_markup=go_back_main_menu)
 
 
-
 #Телефон и интернет
-
 def telefon_internet(call):
     bot.send_message(call.message.chat.id,
                      f"Где купить сим карту, какие есть выгодные тарифы на мобильную связь, как подключить домашний проводной интернет\. "
                      , parse_mode='MarkdownV2', reply_markup=go_back_main_menu)
-
-
-
 
 
 #Транспорт
@@ -294,7 +206,6 @@ def tranport_rules(call):
 
 
 #Переходим в чат GPT или отвечаем простым сообщением если выключен
-
 def chat_gpt (call): #Эта функция для разрешения чата GPT
     global chat_GPT_switch
 
@@ -313,45 +224,34 @@ def chat_gpt (call): #Эта функция для разрешения чата
 
 
 # Блок чата GPT
-
 @bot.message_handler(func=lambda message: True)
 def response_gpt_message (call):
-    global user_id, mess_id, chat_id, bot_mess_id, notes_users
+
     #global chat_GPT_switch
     mes = call.text
     if chat_GPT_switch==True:
 
         print(mes)
         client = Client()
-        response = client.chat.completions.create(
-            model=g4f.models.default,
-            messages=[{"role": "user", "content": mes}],
-            provider= g4f.Provider.Blackbox
-            # Add any other necessary parameters
-        )
-        resp_gpt = (response.choices[0].message.content)
-        bot.send_message(call.chat.id,f'{resp_gpt} \n\n'
-                                         f'Для главного меню нажмите /menu',)
+        try:
+            response = client.chat.completions.create(
+                model=g4f.models.default,
+                messages=[{"role": "user", "content": mes}],
+                provider= g4f.Provider.Blackbox
+                # Add any other necessary parameters
+            )
+            resp_gpt = (response.choices[0].message.content)
+            bot.send_message(call.chat.id,f'{resp_gpt} \n\n Для главного меню нажмите /menu',)
+        except Exception:
+            bot.send_message(call.chat.id, f'Упс, что то пошло не по плану, попробуйте позже \n Для главного меню нажмите /menu', )
         print (resp_gpt)
         print (response)
     else:
         bot_msg = bot.send_message(call.chat.id, f'Нажми /start что бы начать \nНажми /menu для перехода в основное меню '
                                    , parse_mode='MarkdownV2')
-        #bot_msg = bot.send_message(call.chat.id, f'{text_main_welcome_menu}'
-        #                           , parse_mode='MarkdownV2', reply_markup=main_menu)
 
-        user_id=call.from_user.first_name
-        chat_id = call.chat.id
-        mess_id = call.message_id
-        bot_mess_id = bot_msg.message_id
-
-        #print(mess_id)
-        #print(user_id)
-        #print(call)
-
+        #  функция запоминания id сообщений которые послал бот mes_clear_add_list(значение chat.id, значение message_id)
+        mes_clear_add_list(bot_msg.chat.id,bot_msg.message_id)  # Функция для добавления id сообщений в список для удаления
 
 
 bot.polling(none_stop=True)
-
-
-
